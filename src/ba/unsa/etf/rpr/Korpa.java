1c5d8aed4748fee5dec33ad49e3434a54933feb9
eseremet1 <eseremet1@etf.unsa.ba>
2020-10-30 19:35:04 +0100

    Uradjen zadatak 4

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,44 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class Korpa {
+    Artikl[] korpa = new Artikl[50];
+    int brojArtikalaUKorpi;
+
+    public boolean dodajArtikl(Artikl artikl) {
+        if(brojArtikalaUKorpi==50) return false;
+        korpa[brojArtikalaUKorpi] = artikl;
+        brojArtikalaUKorpi = brojArtikalaUKorpi + 1;
+        return true;
+    }
+
+    public Artikl[] getArtikli() {
+
+        return korpa;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+
+        for(int i=0; i<korpa.length; i++) {
+            if(korpa[i] != null && korpa[i].getKod().equals(kod)) {
+                Artikl pomocni = korpa[i];
+                korpa[i] = null;
+                brojArtikalaUKorpi = brojArtikalaUKorpi - 1;
+                return pomocni;
+            }
+        }
+
+        return null;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int ukupnaCijena = 0;
+        for (Artikl artikl: korpa) {
+            if(artikl != null)
+            ukupnaCijena = ukupnaCijena + artikl.getCijena();
+
+        }
+        return ukupnaCijena;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,33 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class Supermarket {
+    Artikl[] supermarket = new Artikl[1000];
+    int brojArtikalaUSupermarketu;
+
+    public Artikl[] getArtikli() {
+        return supermarket;
+    }
+
+    public void dodajArtikl(Artikl artikl) {
+        if(brojArtikalaUSupermarketu==1000) throw new IllegalArgumentException();
+        supermarket[brojArtikalaUSupermarketu] = artikl;
+        brojArtikalaUSupermarketu = brojArtikalaUSupermarketu + 1;
+
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+
+        for(int i=0; i<supermarket.length; i++) {
+            if(supermarket[i] != null && supermarket[i].getKod().equals(kod)) {
+                Artikl pomocni = supermarket[i];
+                supermarket[i] = null;
+                brojArtikalaUSupermarketu = brojArtikalaUSupermarketu - 1;
+                return pomocni;
+            }
+        }
+
+        return null;
+    }
 }
